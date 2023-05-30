e6ea8b55e466fa754d70d9f7339519ea19d06749
Dmitry Sotnikov <charon16822@gmail.com>
2023-05-30 23:19:55 +0300

    add task_5

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-// Напишите функцию,которая ожидает массив элементов в качестве первого аргумента , и перемещает все нули в конец массива
+// Напишите функцию, которая вернет строку с именами студентов , у которых оценка больше 50
 
-const moveZeros = (arr ) => {
-  const arrZero = [];
+const students = [
+  { name: "Alex", mark: "76" },
+  { name: "John", mark: "50" },
+  { name: "Max", mark: "30" },
+  { name: "Dima", mark: "62" },
+  { name: "Sam", mark: "97" },
+];
 
-  const arrFilter = arr.filter((item) => {
-    if (item === 0) {
-      arrZero.push(item);
+const goodMarks = (arr=[]) => {
+  const result=arr.map(item=>{
+    if(item.mark>50){
+      item=item.name
+      return item
     }
-    return item !== 0;
-  });
+  }).filter(item=>item).join(' ')
 
-  return arrFilter.concat(arrZero);
+  return result
 };
